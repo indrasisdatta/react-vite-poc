a48deaa9f27163b720664f595b092cfd57ce44f8
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-06-30 11:00:38 +0530

    Dark, light theme functionality based on default OS settings and switch to toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
 import { AppRouter } from "./AppRouter";
 import { BrowserRouter, NavLink, Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "./Providers/ThemeProvider";
+import { ThemeSwitch } from "./common/components/ThemeSwitch";
 
 const queryclient = new QueryClient();
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryclient}>
-        <header className="bg-black">
-          <nav className="mx-auto flex justify-content-between p-3 lg:px-8">
-            <div className="flex items-center flex-shrink-0 text-white mr-6 hover:bg-gray-700">
-              <NavLink to="/" className="text-white">
-                Vite App
-              </NavLink>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div className="text-sm lg:flex-grow">
+    <ThemeProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryclient}>
+          <header className="bg-white dark:bg-dark ">
+            <nav className="mx-auto flex justify-content-between p-3 lg:px-8">
+              <div className="flex items-center flex-shrink-0 text-black dark:text-white mr-6 hover:bg-gray-700">
                 <NavLink
-                  to="product"
-                  className="block mt-1 md:mt-4 lg:inline-block lg:mt-0 text-teal-100 hover:text-white mr-4"
+                  to="/"
+                  className="text-blue-800 font-bold dark:text-white"
                 >
-                  Product
+                  Vite App
                 </NavLink>
               </div>
-            </div>
-          </nav>
-        </header>
-        <AppRouter />
-        <Outlet />
-      </QueryClientProvider>
-    </BrowserRouter>
+              <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="text-sm lg:flex-grow">
+                  <NavLink
+                    to="product"
+                    className="block mt-1 md:mt-4 lg:inline-block lg:mt-0 text-black dark:text-teal-100 dark:hover:text-white mr-4"
+                  >
+                    Product
+                  </NavLink>
+                </div>
+              </div>
+              <div>
+                <ThemeSwitch />
+              </div>
+            </nav>
+          </header>
+          <AppRouter />
+          <Outlet />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
