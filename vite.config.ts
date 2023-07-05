6889c55f0acb8cf218bb68a09ed36aa35c27d96e
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-07-05 10:20:04 +0530

    Vite unit testing setup and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts'
+  },
   optimizeDeps: {
     exclude: ['react-hook-form'],
   },
